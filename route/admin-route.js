e79deb6f67e47288b4058890a15e57a0bf617788
phcihaya46 <153066205+phcihaya46@users.noreply.github.com>
2024-03-10 17:49:29 +0700

    Add files via upload

--- route/admin-route.js
+++ route/admin-route.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const adminController = require("../controllers/admin-controller");
 const router = express.Router();
+const authenticate = require('../middlewares/authenticate')
 
 router.get('/user', adminController.getUser)
+router.get('/all-status', authenticate, adminController.getAllStatus)
+
+router.put('/updateType/:id',adminController.updateTodo)/////
+router.patch('/updateproduct/:id', adminController.updateProduct) ////
+
+
+router.delete('/deleteProduct/:id',authenticate,adminController.deleteProduct)
+router.delete('/:id',authenticate,adminController.deleteTodo)
+
+router.post('/protype',authenticate,adminController.protype)
+
+router.get('/getProList',adminController.getProList)
 
 module.exports = router;
--- route/auth-route.js
+++ route/auth-route.js
@@ -2,9 +2,41 @@ const express = require('express')
 const router = express.Router()
 const authenticate =require('../middlewares/authenticate')
 const authController = require('../controllers/auth-controller')
+const  productController = require('../controllers/product-controller')
+const  productType = require('../controllers/productType')
+
 
 router.post('/register', authController.register)
 router.post('/login', authController.login)
 router.get('/me', authenticate, authController.getme) 
+router.get('/getCartByUser', authenticate, authController.getCartByUser) 
+router.get('/getproduct/:id',  authController.getProductById) 
+
+router.put('/:id', authenticate, authController.updateUser) 
+
+
+router.post('/cartproduct',authenticate,productController.cartproduct)
+router.post('/cartproduct1',authenticate,productController.detail)
+// router.post('/orderdetail',authenticate,productController.orderdetail)//////
+router.get('/getCartItems',authenticate,productController.getCartItems)
+
+router.post('/protype',authenticate,productType.createProductType)
+router.post('/create',authenticate,productController.createproduct)
+router.get('/product',authenticate,productController.getmenutems)
+
+
+router.delete('/deleteCart/:productId',authenticate,productController.deleteCart)
+
+router.post('/orderProduct',authenticate,productController.orderProduct)
+
+
+
+router.get('/category/:protypeId', authController.getBooksByCategory);
+router.get('/AddmingetOrder', authController.AddmingetOrder);
+
+router.get('/getOrder', authenticate, productController.getOrder)
+// router.get('/getproduct/:id', authenticate, productController.getPro)
+
+
 
 module.exports = router
